6c443159494cb6e158777b43443fec97a5f4a9ba
Ajeet Singh <ajeetsingh14121971@gmail.com>
2017-06-14 15:09:20 +0530

    new

---
+++ Token/app/router/api.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app=express();
+var apiRoutes = express.Router();
+
+apiRoutes.get('/', function(req, res) {
+    res.json({ message: 'Welcome to the coolest API on earth!' });
+});
+
+apiRoutes.get('/users', function(req, res) {
+    User.find({}, function(err, users) {
+        res.json(users);
+    });
+});
+
+app.use('/api', apiRoutes);
---
+++ Token/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    'secret': 'lanetteam',
+    'database': 'mongodb://localhost/token'
+};
